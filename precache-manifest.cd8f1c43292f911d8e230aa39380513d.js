be6b9fd36a028b36b527d0f2bfcca06a5bccfda1
krzysiek <krzychu33b@gmail.com>
2020-05-05 06:35:02 +0200

    Updates

---
+++ precache-manifest.cd8f1c43292f911d8e230aa39380513d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "d9d36b65a132d26aed884ad58e4719eb",
+    "url": "/app-rr/static/media/header3.d9d36b65.jpg"
+  },
+  {
+    "revision": "33eca35fceea8311d67358a6629fa9d9",
+    "url": "/app-rr/static/media/header2.33eca35f.jpg"
+  },
+  {
+    "revision": "70e6670ee746fc59f414fde4fcce93af",
+    "url": "/app-rr/static/media/header1.70e6670e.jpg"
+  },
+  {
+    "revision": "9347ed20ee693a8d157a",
+    "url": "/app-rr/static/js/runtime~main.fe775957.js"
+  },
+  {
+    "revision": "205c806c00fb45b3d789",
+    "url": "/app-rr/static/js/main.f9a0d37b.chunk.js"
+  },
+  {
+    "revision": "d2a4e9d1dcdd50428e49",
+    "url": "/app-rr/static/js/2.1f6191ca.chunk.js"
+  },
+  {
+    "revision": "205c806c00fb45b3d789",
+    "url": "/app-rr/static/css/main.58ffaaf7.chunk.css"
+  },
+  {
+    "revision": "31eab10dcbfb0c61dfe4259b41e22bcf",
+    "url": "/app-rr/index.html"
+  }
+];
